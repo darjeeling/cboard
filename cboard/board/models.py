d45f5ea49acd7e26a352c6ed80c37f7abbc79ae7
Bae KwonHan <darjeeling@gmail.com>
2018-01-05 00:02:46 +0900

    modify post model and start to add test

--- cboard/board/models.py
+++ cboard/board/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Posts(models.Model):
+class Post(models.Model):
     subject = models.CharField(max_length=255)
     contents = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.PROTECT)
     create_datetime = models.DateTimeField(auto_now_add=True)
-    ip_address = models.IPAddressField()
+    ip_address = models.GenericIPAddressField()
     comment_count = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
     class Meta:
--- cboard/board/tests.py
+++ cboard/board/tests.py
@@ -1,6 +1,12 @@
 import pytest
+from model_mommy import mommy
+from model_mommy.recipe import Recipe, foreign_key
 
 @pytest.mark.django_db
 def test_test():
-    from board.models import Posts
+    from board.models import Post
+    user = mommy.make('User', username='test1', is_active=True)
+    user.set_password('test')
+    user.save()
+    post = mommy.make('Post')
     assert 1 == 1
