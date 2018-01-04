6157e325ddacb9e4b6641f388a8f94f0497b0d4e
Bae KwonHan <darjeeling@gmail.com>
2018-01-04 23:50:37 +0900

    add posts model

--- cboard/board/models.py
+++ cboard/board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Posts(models.Model):
+    subject = models.CharField(max_length=255)
+    contents = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.PROTECT)
+    create_datetime = models.DateTimeField(auto_now_add=True)
+    ip_address = models.IPAddressField()
+    comment_count = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    class Meta:
+        app_label = 'board'
--- cboard/board/tests.py
+++ cboard/board/tests.py
@@ -2,4 +2,5 @@ import pytest
 
 @pytest.mark.django_db
 def test_test():
+    from board.models import Posts
     assert 1 == 1
